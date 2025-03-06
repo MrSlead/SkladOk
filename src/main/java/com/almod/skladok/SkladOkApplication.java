7d8b0adc99a7986a752d99f5a6bb7f5cdd042f05
mordy <mordyasov.60@mail.ru>
2025-03-06 18:17:32 +0700

    Инициализация проекта

---
+++ src/main/java/com/almod/skladok/SkladOkApplication.java
@@ -0,0 +1,13 @@
+package com.almod.skladok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkladOkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkladOkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almod/skladok/SkladOkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almod.skladok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkladOkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
